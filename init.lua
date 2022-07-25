8f244b753f948cb74f28797c1c250312ae5a1597
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2022-07-25 11:37:34 +0200

    using gruvbox-baby again

--- init.lua
+++ init.lua
@@ -67,95 +67,16 @@ vim.cmd([[
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 	use({
-		"catppuccin/nvim",
-		as = "catppuccin",
+		"luisiacc/gruvbox-baby",
 		config = function()
-			local catppuccin = require("catppuccin")
-			catppuccin.setup({
-				transparent_background = false,
-				term_colors = true,
-				styles = {
-					comments = "italic",
-					conditionals = "NONE",
-					loops = "NONE",
-					functions = "bold",
-					keywords = "bold",
-					strings = "NONE",
-					variables = "NONE",
-					numbers = "NONE",
-					booleans = "NONE",
-					properties = "NONE",
-					types = "NONE",
-					operators = "NONE",
-				},
-				integrations = {
-					treesitter = true,
-					native_lsp = {
-						enabled = true,
-						virtual_text = {
-							errors = "italic",
-							hints = "italic",
-							warnings = "italic",
-							information = "italic",
-						},
-						underlines = {
-							errors = "underline",
-							hints = "underline",
-							warnings = "underline",
-							information = "underline",
-						},
-					},
-					coc_nvim = false,
-					lsp_trouble = true,
-					cmp = true,
-					lsp_saga = false,
-					gitgutter = false,
-					gitsigns = true,
-					telescope = true,
-					nvimtree = {
-						enabled = true,
-						show_root = true,
-						transparent_panel = true,
-					},
-					neotree = {
-						enabled = false,
-						show_root = false,
-						transparent_panel = false,
-					},
-					which_key = true,
-					indent_blankline = {
-						enabled = true,
-						colored_indent_levels = true,
-					},
-					dashboard = true,
-					neogit = true,
-					vim_sneak = false,
-					fern = false,
-					barbar = false,
-					bufferline = true,
-					markdown = false,
-					lightspeed = false,
-					ts_rainbow = true,
-					hop = false,
-					notify = true,
-					telekasten = false,
-					symbols_outline = true,
-				},
-			})
-			vim.cmd([[colorscheme catppuccin]])
+			vim.g.gruvbox_baby_comment_style = "italic"
+			vim.g.gruvbox_baby_keyword_style = "bold"
+			vim.g.gruvbox_baby_function_style = "italic"
+			vim.g.gruvbox_baby_variable_style = "NONE"
+			vim.g.gruvbox_baby_telescope_theme = 1
+			vim.cmd([[colorscheme gruvbox-baby]])
 		end,
 	})
-	-- use({
-	-- 	"luisiacc/gruvbox-baby",
-	-- 	config = function()
-	-- 		vim.g.gruvbox_baby_comment_style = "italic"
-	-- 		vim.g.gruvbox_baby_keyword_style = "bold"
-	-- 		vim.g.gruvbox_baby_function_style = "italic"
-	-- 		vim.g.gruvbox_baby_variable_style = "NONE"
-	-- 		vim.g.gruvbox_baby_telescope_theme = 1
-	-- 		vim.cmd([[colorscheme gruvbox-baby]])
-	-- 	end,
-	-- })
 	use({
 		"windwp/nvim-autopairs",
 		config = function()
@@ -501,7 +422,7 @@ return require("packer").startup(function(use)
 		config = function()
 			require("lualine").setup({
 				options = {
-					theme = "catppuccin", -- "gruvbox-baby",
+					theme = "gruvbox-baby",
 				},
 				sections = {
 					lualine_c = {
