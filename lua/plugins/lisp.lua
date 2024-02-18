00651bf2b2dda0c4065b76ebeff3bfe54ee653d6
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2024-02-18 18:05:47 +0100

    add parpar (parinfer and paredit)

---
+++ lua/plugins/lisp.lua
@@ -0,0 +1,6 @@
+return {
+	ft = { "commonlisp", "scheme", "fennel", "clojure", "racket" },
+	"dundalek/parpar.nvim",
+	dependencies = { "gpanders/nvim-parinfer", "julienvincent/nvim-paredit" },
+	opts = {},
+}
