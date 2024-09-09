965e56f0a93804c51360eec1dd66629c40a20fdf
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2024-09-09 22:33:45 +0200

    add snippets back

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,22 +8,30 @@ return {
 		"ray-x/cmp-treesitter",
 		"hrsh7th/cmp-nvim-lsp-signature-help",
 		"https://codeberg.org/FelipeLema/cmp-async-path",
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			build = "make install_jsregexp",
+			dependencies = { "saadparwaiz1/cmp_luasnip" },
+		},
 	},
 	config = function()
 		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		require("luasnip.loaders.from_vscode").lazy_load()
 		local lspkind = require("lspkind")
-
-		local has_words_before = function()
-			unpack = unpack or table.unpack
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
 
 		cmp.setup({
 			window = {
 				completion = cmp.config.window.bordered(),
 				documentation = cmp.config.window.bordered(),
 			},
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+
 			formatting = {
 				format = lspkind.cmp_format({
 					mode = "symbol",
@@ -34,33 +42,42 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<Tab>"] = function(fallback)
-					if not cmp.select_next_item() then
-						if vim.bo.buftype ~= "prompt" and has_words_before() then
-							cmp.complete()
+				-- ["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						if luasnip.expandable() then
+							luasnip.expand()
 						else
-							fallback()
+							cmp.confirm({
+								select = true,
+							})
 						end
+					else
+						fallback()
 					end
-				end,
-				["<S-Tab>"] = function(fallback)
-					if not cmp.select_prev_item() then
-						if vim.bo.buftype ~= "prompt" and has_words_before() then
-							cmp.complete()
-						else
-							fallback()
-						end
+				end),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.locally_jumpable(1) then
+						luasnip.jump(1)
+					else
+						fallback()
 					end
-				end,
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
-				{
-					name = "nvim_lsp",
-					entry_filter = function(entry)
-						return cmp.lsp.CompletionItemKind.Snippet ~= entry:get_kind()
-					end,
-				},
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
 				{ name = "nvim_lsp_signature_help" },
 				{ name = "buffer" },
 				{ name = "treesitter" },
