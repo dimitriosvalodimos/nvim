43c7bcbc9de157b0ce3f4dfbd621c3102f272ac5
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2024-09-14 23:04:44 +0200

    disable testing until I understand how it works

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -1,9 +1,24 @@
-return {
-	"nvim-neotest/neotest",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
-}
+-- return {
+-- 	"nvim-neotest/neotest",
+-- 	dependencies = {
+-- 		"nvim-neotest/nvim-nio",
+-- 		"nvim-lua/plenary.nvim",
+-- 		"antoinemadec/FixCursorHold.nvim",
+-- 		"nvim-treesitter/nvim-treesitter",
+-- 		"nvim-neotest/neotest-go",
+-- 	},
+-- 	config = function()
+-- 		local neotest_ns = vim.api.nvim_create_namespace("neotest")
+-- 		vim.diagnostic.config({
+-- 			virtual_text = {
+-- 				format = function(diagnostic)
+-- 					local msg = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+-- 					return msg
+-- 				end,
+-- 			},
+-- 		}, neotest_ns)
+-- 		require("neotest").setup({
+-- 			adapters = { require("neotest-go") },
+-- 		})
+-- 	end,
+-- }
