27e9a2a8176042cb5ed5b4492aba75122bab34bc
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2024-09-16 23:47:56 +0200

    add some extra settings to cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -93,6 +93,13 @@ return {
 					end
 				end,
 			}),
+			matching = {
+				disallow_fuzzy_matching = true,
+				disallow_fullfuzzy_matching = true,
+				disallow_partial_fuzzy_matching = true,
+				disallow_partial_matching = true,
+				disallow_prefix_unmatching = false,
+			},
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "treesitter" },
