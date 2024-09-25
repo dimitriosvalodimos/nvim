c72483ece8ec7c35c0707ce38ab25c8bb0071bc2
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2024-09-25 20:24:31 +0200

    reset formatting[C

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,31 +1,22 @@
 return {
 	"stevearc/conform.nvim",
 	event = "VeryLazy",
-	config = function()
-		local conform = require("conform")
-		local in_kilanka = function()
-			if string.find(vim.fn.expand("%:p"), "kilanka") then
-				return { "prettier", "biome-check", "biome", stop_after_first = true }
-			end
-			return { "biome-check", "biome", "prettier", stop_after_first = true }
-		end
-		conform.setup({
-			notify_on_error = false,
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-			formatters_by_ft = {
-				css = in_kilanka(),
-				go = { "goimports", "golines", "gofumpt" },
-				html = in_kilanka(),
-				javascript = in_kilanka(),
-				javascriptreact = in_kilanka(),
-				json = in_kilanka(),
-				lua = { "stylua" },
-				typescript = in_kilanka(),
-				typescriptreact = in_kilanka(),
-			},
-		})
-	end,
+	opts = {
+		notify_on_error = false,
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+		formatters_by_ft = {
+			css = { "prettier", "biome-check", "biome", stop_after_first = true },
+			go = { "goimports", "golines", "gofumpt" },
+			html = { "prettier", "biome-check", "biome", stop_after_first = true },
+			javascript = { "prettier", "biome-check", "biome", stop_after_first = true },
+			javascriptreact = { "prettier", "biome-check", "biome", stop_after_first = true },
+			json = { "prettier", "biome-check", "biome", stop_after_first = true },
+			lua = { "stylua" },
+			typescript = { "prettier", "biome-check", "biome", stop_after_first = true },
+			typescriptreact = { "prettier", "biome-check", "biome", stop_after_first = true },
+		},
+	},
 }
