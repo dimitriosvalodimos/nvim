619b7bbb15efa00d48dd98b3fb4880487aff610c
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2024-10-03 23:36:44 +0200

    add tabline

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -4,8 +4,16 @@ return {
 	opts = {
 		options = {
 			theme = "iceberg_dark",
-			tabline = {},
-			winbar = {},
+			section_separators = "",
+			component_separators = "",
+		},
+		tabline = {
+			lualine_a = { "buffers" },
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = { "tabs" },
 		},
 	},
 }
