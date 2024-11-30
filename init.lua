a9034c8ff0b3d32cda11de3bf67b3601a5bece32
Dimitrios Valodimos <dimi@Dimitrioss-MacBook-Pro.fritz.box>
2024-11-30 14:50:43 +0100

    remove themes

--- init.lua
+++ init.lua
@@ -2,4 +2,3 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 require("config.cmds")
-vim.cmd.colorscheme("vesper") -- gruber-darker, no-clown-fiesta, oxocarbon, iceclimber, roseprime, vesper
