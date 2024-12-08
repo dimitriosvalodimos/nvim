83966b9ba915ed5304976b86cec2319681186df2
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2024-12-08 14:42:41 +0100

    add helpview highlights

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,3 +1,4 @@
 return {
 	{ "OXY2DEV/markview.nvim", dependencies = { "nvim-treesitter/nvim-treesitter" }, opts = {} },
+	{ "OXY2DEV/helpview.nvim", dependencies = { "nvim-treesitter/nvim-treesitter" }, opts = {} },
 }
