e8b66db332cb9b83ef88700064b1d18f9e311b90
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-01-06 20:44:55 +0100

    add better default linetheme

--- lua/plugins/lines.lua
+++ lua/plugins/lines.lua
@@ -1,5 +1,5 @@
 return {
-	{ "nvim-lualine/lualine.nvim", opts = { options = { theme = "auto" } } },
+	{ "nvim-lualine/lualine.nvim", opts = { options = { theme = "modus-vivendi" } } },
 	{ "akinsho/bufferline.nvim", version = "*", opts = { options = { close_icon = "", buffer_close_icon = "" } } },
 	{ "Bekaboo/dropbar.nvim" },
 }
