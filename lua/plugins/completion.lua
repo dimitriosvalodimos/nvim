be27a019413871b320ecc4c6a98bbdfed4f1ba57
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-01-07 20:52:41 +0100

    remove auto pairs

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,14 +1,10 @@
 return {
 	{
-		"windwp/nvim-autopairs",
-		opts = { enable_check_bracket_line = false, disable_filetype = { "spectre_panel", "vim" } },
-	},
-	{
 		"saghen/blink.cmp",
 		lazy = false,
 		version = "v0.*",
 		opts = {
-			keymap = { preset = "super-tab" }, -- default, super-tab, enter
+			keymap = { preset = "enter" }, -- default, super-tab, enter
 			appearance = {
 				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "normal", -- mono, normal
