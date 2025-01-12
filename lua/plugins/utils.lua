9ce5f06c93ef27736d1ba94503be7a549c5f2531
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-01-12 20:35:18 +0100

    fix filetype for markview

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,4 +1,4 @@
 return {
-	{ "OXY2DEV/markview.nvim", dependencies = { "nvim-treesitter/nvim-treesitter" }, opts = {}, ft = "md" },
+	{ "OXY2DEV/markview.nvim", dependencies = { "nvim-treesitter/nvim-treesitter" }, opts = {}, ft = "markdown" },
 	{ "OXY2DEV/helpview.nvim", dependencies = { "nvim-treesitter/nvim-treesitter" }, opts = {} },
 }
