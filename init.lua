eed54cb4710ffbe3599fe608a572ea7bd9bf4d9e
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-01-27 23:16:19 +0100

    set to monoglow

--- init.lua
+++ init.lua
@@ -2,4 +2,4 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 require("config.cmds")
-vim.cmd.colorscheme("github-monochrome") -- gruber-darker, monoglow/-z/-lack/-void, cold, github-monochrome
+vim.cmd.colorscheme("monoglow") -- gruber-darker, monoglow/-z/-lack/-void, cold, github-monochrome
