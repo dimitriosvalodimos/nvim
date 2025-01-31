55423ee8a3e8d79b06cd3a7f2bd50ecb008e2354
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-01-31 13:48:12 +0100

    use the ol' reliable

--- init.lua
+++ init.lua
@@ -2,4 +2,4 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 require("config.cmds")
-vim.cmd.colorscheme("oldworld") -- lackluster-(hack, mint, night, dark), gruber-darker, no-clown-fiesta, oldworld
+vim.cmd.colorscheme("gruber-darker") -- lackluster-(hack, mint, night, dark), gruber-darker, no-clown-fiesta, oldworld
