89eec7f8bf7f0455d1d51e43e533f54640402fcd
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-02-04 22:02:32 +0100

    add vague

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -6,5 +6,6 @@ return {
 		opts = { italic = { strings = false, comments = false, folds = false } },
 	},
 	{ "aktersnurra/no-clown-fiesta.nvim", lazy = true, priority = 1000, opts = {} },
+	{ "vague2k/vague.nvim", lazy = true, priority = 1000, opts = {} },
 	{ "mellow-theme/mellow.nvim", lazy = true, priority = 1000 },
 }
