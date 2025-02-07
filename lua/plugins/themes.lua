93d6059a8b72b962785f58242b157d74472611c9
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-02-07 17:39:52 +0100

    readd lualine, add vesper

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,8 +1,11 @@
 return {
-	{
-		"blazkowolf/gruber-darker.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = { italic = { strings = false, comments = false, folds = false } },
-	},
+  {
+    "blazkowolf/gruber-darker.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = { italic = { strings = false, comments = false, folds = false } },
+  },
+  { "bettervim/yugen.nvim",   lazy = true, priority = 1000, opts = {} },
+  { "ficcdaf/ashen.nvim",     lazy = true, priority = 1000, opts = {} },
+  { "datsfilipe/vesper.nvim", lazy = true, priority = 1000, opts = { italics = {} } }
 }
