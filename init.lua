86146c058d6399a2a419e4714137dbe1a65bc30d
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-02-09 14:29:49 +0100

    add popup

--- init.lua
+++ init.lua
@@ -2,4 +2,4 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 require("config.cmds")
-vim.cmd.colorscheme("gruber-darker") -- gruber-darker
+vim.cmd.colorscheme("gruber-darker")
