989942f8fdb00a2e74a5c52b08ece3ea900d2af1
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-03-16 20:47:30 +0100

    less themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -6,5 +6,4 @@ return {
 		opts = { invert = { signs = true }, italic = { strings = false, comments = false } },
 	},
 	{ "rockyzhang24/arctic.nvim", branch = "v2", dependencies = { "rktjmp/lush.nvim" }, lazy = true, priority = 1000 },
-	{ "nyoom-engineering/oxocarbon.nvim", lazy = true, priority = 1000 },
 }
