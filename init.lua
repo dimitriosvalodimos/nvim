f7253593398e72d48285d5bce11f3601a2bf45c4
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-05-04 10:24:15 +0200

    remove unused lsps, back to basics...again

--- init.lua
+++ init.lua
@@ -1,16 +1,32 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
 local g = vim.g
 local opt = vim.opt
 g.mapleader = " "
 g.maplocalleader = ";"
-opt.background = "dark" -- dark, light
 opt.backupcopy = "yes"
 opt.breakindent = true
 opt.clipboard:append("unnamedplus")
--- opt.cmdheight = 0
 opt.completeopt = { "menu", "menuone", "noselect" }
 opt.cursorline = true
 opt.expandtab = true
+opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+opt.grepformat = "%f:%l:%c:%m,%f:%l:%m"
 opt.ignorecase = true
+opt.inccommand = "nosplit"
 opt.infercase = true
 opt.laststatus = 2
 opt.number = true
@@ -28,7 +44,6 @@ opt.splitbelow = true
 opt.splitright = true
 opt.tabstop = 2
 opt.termguicolors = true
-opt.winborder = "single"
 opt.wrap = false
 local function map(mode, lhs, rhs, opts)
 	if type(opts) == "string" then
@@ -37,28 +52,8 @@ local function map(mode, lhs, rhs, opts)
 		vim.keymap.set(mode, lhs, rhs, opts)
 	end
 end
-map("v", "<", "<gv", "dedent")
-map("v", ">", ">gv", "indent")
-map("i", "<A-u>", "<c-r>=trim(system('uuidgen'))<cr>", "uuid")
-map("n", "<A-u>", "i<c-r>=trim(system('uuidgen'))<cr><esc>", "uuid")
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-vim.opt.rtp:prepend(lazypath)
 local servers = {
 	cssls = {},
-	eslint = {},
 	html = {},
 	lua_ls = {
 		settings = {
@@ -71,11 +66,13 @@ local servers = {
 			},
 		},
 	},
-	rust_analyzer = {},
-	svelte = {},
 	ts_ls = {},
 }
 local diagnostic_config = { severity_sort = true, virtual_lines = false, virtual_text = true }
+map("v", "<", "<gv", "dedent")
+map("v", ">", ">gv", "indent")
+map("i", "<A-u>", "<c-r>=trim(system('uuidgen'))<cr>", "uuid")
+map("n", "<A-u>", "i<c-r>=trim(system('uuidgen'))<cr><esc>", "uuid")
 map("n", "<leader>k", function()
 	vim.diagnostic.config({ virtual_lines = { current_line = true }, virtual_text = false })
 	vim.api.nvim_create_autocmd("CursorMoved", {
@@ -87,20 +84,11 @@ map("n", "<leader>k", function()
 	})
 end)
 require("lazy").setup({
-	{ "Mofiqul/vscode.nvim", lazy = true, priority = 1000, opts = { transparent = false, italic_comments = false } },
 	{
 		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
 		lazy = false,
-		keys = { { "<leader>gs", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "toggle git blame" } },
+		opts = {},
+		keys = { { "<leader>gs", ":Gitsigns toggle_current_line_blame<cr>", desc = "git blame" } },
 	},
 	{
 		"stevearc/oil.nvim",
@@ -118,15 +106,11 @@ require("lazy").setup({
 		version = "1.*",
 		opts = {
 			keymap = { preset = "enter" },
-			completion = {
-				documentation = { auto_show = true },
-				menu = { draw = { columns = { { "label", "label_description", gap = 1 }, { "kind" } } } },
-			},
+			completion = { documentation = { auto_show = true } },
 			signature = { enabled = true },
 			sources = { default = { "lsp", "path", "snippets", "buffer" } },
 			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
-		opts_extend = { "sources.default" },
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -142,54 +126,25 @@ require("lazy").setup({
 	},
 	{
 		"ibhagwan/fzf-lua",
-		opts = { { "ivy" }, winopts = { border = "none", preview = { border = "none" } } },
+		opts = {},
 		keys = {
 			{ "<leader>ff", ":silent FzfLua files<cr>", desc = "find files" },
 			{ "<leader>fr", ":silent FzfLua resume<cr>", desc = "resume search" },
 			{ "<leader>fb", ":silent FzfLua buffers<cr>", desc = "find buffer" },
 			{ "<leader>fg", ":silent FzfLua live_grep<cr>", desc = "find word" },
-			{ "<leader>fh", ":silent FzfLua helptags<cr>", desc = "find helptag" },
-			{ "<leader>fk", ":silent FzfLua keymaps<cr>", desc = "find keymap" },
-			{ "<leader>/", ":silent FzfLua lgrep_curbuf<cr>", desc = "find in buffer" },
 		},
 	},
 	{
 		"williamboman/mason.nvim",
-		dependencies = { "neovim/nvim-lspconfig", "saghen/blink.cmp", "smjonas/inc-rename.nvim" },
+		dependencies = { "neovim/nvim-lspconfig", "saghen/blink.cmp" },
 		config = function()
 			local lspconfig = require("lspconfig")
 			require("mason").setup()
-			require("inc_rename").setup()
 			local capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
-			vim.diagnostic.config({ severity_sort = true, virtual_text = true })
-
+			vim.diagnostic.config(diagnostic_config)
 			for server, config in pairs(servers) do
-				lspconfig[server].setup({
-					capabilities = capabilities,
-					flags = { debounce_text_changes = 150 },
-					settings = config.settings or {},
-					init_options = config.init_options or {},
-				})
+				lspconfig[server].setup({ capabilities = capabilities, settings = config.settings or {} })
 			end
-
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("config-lsp-attach", { clear = true }),
-				callback = function(event)
-					local buf = event.buf
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client:supports_method("textDocument/documentColor") then
-						vim.lsp.document_color.enable(true, buf, { style = "virtual" })
-					end
-					map("n", "<leader>rn", vim.lsp.buf.rename, { buffer = buf })
-					map("n", "gr", vim.lsp.buf.references, { buffer = buf })
-					map("n", "gd", vim.lsp.buf.definition, { buffer = buf })
-					map("n", "gD", vim.lsp.buf.type_definition, { buffer = buf })
-					map("n", "gI", vim.lsp.buf.implementation, { buffer = buf })
-					map("n", "gcI", vim.lsp.buf.incoming_calls, { buffer = buf })
-					map("n", "gcO", vim.lsp.buf.outgoing_calls, { buffer = buf })
-					map("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = buf })
-				end,
-			})
 		end,
 	},
 	{ "xemptuous/sqlua.nvim", lazy = true, cmd = "SQLua", opts = {} },
@@ -197,19 +152,19 @@ require("lazy").setup({
 	{
 		"stevearc/conform.nvim",
 		opts = {
-			format_on_save = { lsp_format = true, async = false },
+			format_on_save = { lsp_format = true, async = false, stop_after_first = true },
 			formatters_by_ft = {
-				css = { "biome-check", "prettier", stop_after_first = true, lsp_format = "fallback" },
-				html = { "biome-check", "prettier", stop_after_first = true, lsp_format = "fallback" },
-				javascript = { "biome-check", "prettier", stop_after_first = true, lsp_format = "fallback" },
-				javascriptreact = { "biome-check", "prettier", stop_after_first = true, lsp_format = "fallback" },
-				json = { "biome-check", "prettier", stop_after_first = true, lsp_format = "fallback" },
-				lua = { "stylua", lsp_format = "fallback" },
-				typescript = { "biome-check", "prettier", stop_after_first = true, lsp_format = "fallback" },
-				typescriptreact = { "biome-check", "prettier", stop_after_first = true, lsp_format = "fallback" },
+				css = { "biome-check", "prettier" },
+				html = { "biome-check", "prettier" },
+				javascript = { "biome-check", "prettier" },
+				javascriptreact = { "biome-check", "prettier" },
+				json = { "biome-check", "prettier" },
+				lua = { "stylua" },
+				typescript = { "biome-check", "prettier" },
+				typescriptreact = { "biome-check", "prettier" },
 			},
 		},
 	},
 })
-vim.cmd.colorscheme("vscode") -- default, vscode
+vim.cmd.colorscheme("lunaperche") -- default, lunaperche, retrobox, slate, sorbet
 -- MasonInstall css-lsp html-lsp typescript-language-server lua-language-server stylua prettier
