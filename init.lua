fb54ce1224c4eb273d75d0a3059da703e4c35d7d
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-08-11 21:20:10 +0200

    use fzf wherever possible

--- init.lua
+++ init.lua
@@ -97,26 +97,6 @@ require("lazy").setup({
 		},
 	},
 	{
-		"mason-org/mason.nvim",
-		dependencies = {
-			"saghen/blink.cmp",
-			"neovim/nvim-lspconfig",
-			"zapling/mason-conform.nvim",
-			"mason-org/mason-lspconfig.nvim",
-		},
-		config = function()
-			local capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
-			vim.lsp.config("*", { capabilities = capabilities })
-			vim.diagnostic.config({ severity_sort = true, virtual_lines = false, virtual_text = true })
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "cssls", "html", "lua_ls", "ts_ls" },
-				automatic_enable = true,
-			})
-			require("mason-conform").setup({})
-		end,
-	},
-	{
 		"ibhagwan/fzf-lua",
 		opts = { { "ivy", "hide" } },
 		keys = {
@@ -127,6 +107,34 @@ require("lazy").setup({
 			{ "<leader>/", ":FzfLua grep_curbuf<cr>" },
 			{ "<leader>xx", ":FzfLua diagnostics_document<cr>" },
 		},
+	},
+	{
+		"mason-org/mason.nvim",
+		dependencies = {
+			"saghen/blink.cmp",
+			"ibhagwan/fzf-lua",
+			"neovim/nvim-lspconfig",
+			"zapling/mason-conform.nvim",
+			"mason-org/mason-lspconfig.nvim",
+		},
+		config = function()
+			local fzf = require("fzf-lua")
+			fzf.register_ui_select()
+			local capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
+			vim.lsp.config("*", { capabilities = capabilities })
+			vim.diagnostic.config({ severity_sort = true, virtual_lines = false, virtual_text = true })
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "cssls", "html", "lua_ls", "ts_ls" },
+				automatic_enable = true,
+			})
+			require("mason-conform").setup({})
+			map("n", "gca", fzf.lsp_code_actions, {})
+			map("n", "grr", fzf.lsp_references, {})
+			map("n", "gri", fzf.lsp_implementations, {})
+			map("n", "grt", fzf.lsp_typedefs, {})
+			map("n", "gO", fzf.lsp_document_symbols, {})
+		end,
 	},
 	{
 		"NeogitOrg/neogit",
