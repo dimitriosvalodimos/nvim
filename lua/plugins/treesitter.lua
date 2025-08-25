71c521e047e58a8c41d468e7d4e42b6e51010583
Dimitrios Valodimos <32107659+dimitriosvalodimos@users.noreply.github.com>
2025-08-25 08:27:45 +0200

    Add expand/contract selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
 			auto_install = true,
 			indent = { enable = true },
 			highlight = { enable = true, additional_vim_regex_highlighting = false, use_languagetree = true },
+            incremental_selection = { enable = true, keymaps = { node_incremental = "v", node_decremental = "V" } },
 			ensure_installed = { "diff", "lua", "luadoc", "markdown", "markdown_inline", "vimdoc" },
 		})
 	end,
