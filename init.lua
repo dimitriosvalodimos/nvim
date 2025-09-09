b3859bfbaf184013d55bc33a9aa3a23251dffb02
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-09-09 20:50:33 +0200

    more options and mappings

--- init.lua
+++ init.lua
@@ -1,4 +1,35 @@
-vim.g.mapleader = " "
+local g = vim.g
+local opt = vim.opt
+g.mapleader = " "
+opt.backup = false
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+opt.cursorlineopt = "number"
+opt.expandtab = true
+opt.ignorecase = true
+opt.incsearch = true
+opt.infercase = true
+opt.mouse = "a"
+opt.number = true
+opt.pumheight = 10
+opt.ruler = false
+opt.shiftwidth = 2
+opt.shortmess:append("WcC")
+opt.showmode = false
+opt.signcolumn = "yes"
+opt.smartcase = true
+opt.smartindent = true
+opt.softtabstop = 2
+opt.splitbelow = true
+opt.splitkeep = "screen"
+opt.splitright = true
+opt.tabstop = 2
+opt.termguicolors = true
+opt.undofile = true
+opt.virtualedit = "block"
+opt.wrap = false
+opt.writebackup = false
+vim.cmd("filetype plugin indent on")
 vim.pack.add({
 	"https://github.com/ibhagwan/fzf-lua",
 	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
@@ -8,8 +39,6 @@ vim.pack.add({
 	"https://github.com/stevearc/conform.nvim",
 	"https://github.com/stevearc/oil.nvim",
 })
-
-require("mini.basics").setup()
 require("mini.icons").setup()
 require("mini.tabline").setup()
 require("mini.statusline").setup()
@@ -39,14 +68,12 @@ require("nvim-treesitter.configs").setup({
 })
 require("mini.pairs").setup()
 require("mini.completion").setup()
-
 local servers = { "cssls", "html", "lua_ls", "ts_ls" }
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 vim.diagnostic.config({ severity_sort = true, virtual_lines = false, virtual_text = true })
 vim.lsp.config("*", { capabilities = capabilities })
 vim.lsp.enable(servers)
-
 local prettier_or_biome = function(bufnr)
 	local biome = require("conform").get_formatter_info("biome", bufnr)
 	if biome.available then
@@ -72,19 +99,18 @@ require("mini.git").setup()
 require("mini.move").setup()
 require("oil").setup({ view_options = { show_hidden = true }, columns = { "permissions", "size", "mtime" } })
 require("render-markdown").setup()
-
 local fzf = require("fzf-lua")
 fzf.setup({ "border-fused", "fzf-native", "hide" })
 fzf.register_ui_select()
-
 local map = vim.keymap.set
 map("i", "<A-u>", "<c-r>=trim(system('uuidgen'))<cr>")
 map("n", "<A-u>", "i<c-r>=trim(system('uuidgen'))<cr><esc>")
-map("i", "<C-b>", "<ESC>^i")
-map("i", "<C-e>", "<End>")
 map("n", "<Esc>", "<cmd>noh<CR>")
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>")
 map("n", "-", ":Oil<cr>")
+map("n", "<C-s>", "<cmd>w<CR>")
+map("i", "<C-b>", "<ESC>^i")
+map("i", "<C-e>", "<End>")
 map("n", "<leader>ff", ":FzfLua files<cr>")
 map("n", "<leader>fb", ":FzfLua buffers<cr>")
 map("n", "<leader>fo", ":FzfLua oldfiles<cr>")
@@ -102,3 +128,14 @@ map("n", "gd", ":FzfLua lsp_definitions<cr>")
 map("n", "gri", ":FzfLua lsp_implementations<cr>")
 map("n", "grt", ":FzfLua lsp_typedefs<cr>")
 map("n", "gO", ":FzfLua lsp_document_symbols<cr>")
+map("n", "<c-h>", "<c-w>h")
+map("n", "<c-j>", "<c-w>j")
+map("n", "<c-k>", "<c-w>k")
+map("n", "<c-l>", "<c-w>l")
+local group = vim.api.nvim_create_augroup("config_group", {})
+local au = function(event, pattern, callback, desc)
+	vim.api.nvim_create_autocmd(event, { group = group, pattern = pattern, callback = callback, desc = desc })
+end
+au("TextYankPost", "*", function()
+	vim.hl.on_yank()
+end)
